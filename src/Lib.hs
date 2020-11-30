8680203371d55b39184861878c2f605ce2f79c38
Franco Banfi <francobanfi01@gmail.com>
2020-11-30 17:57:10 -0300

    Practica Parciales viejos

--- src/Lib.hs
+++ src/Lib.hs
@@ -193,3 +193,97 @@ hierros n habilidad = (UnTiro ((fuerzaJugador habilidad)*n) (div (precisionJugad
 
 
 listaDePalos = [putter, madera,(hierros 1),(hierros 2),(hierros 3),(hierros 4),(hierros 5),(hierros 6),(hierros 7),(hierros 8),(hierros 9),(hierros 10)]
+
+-----------------------------------Punto 2--------------------------------------------------
+golpe :: Palo -> Jugador -> Tiro
+golpe palo = palo.habilidad
+
+-----------------------------------Punto 3--------------------------------------------------
+data Obstaculo = UnObstaculo {obstaculo::String,longitud::Int} deriving Show
+
+obstaculoGenerico1 = UnObstaculo {obstaculo="Tunel", longitud=50}         -- PARA LOS TESTS
+obstaculoGenerico2 = UnObstaculo {obstaculo="Tunel", longitud=50}
+obstaculoGenerico3 = UnObstaculo {obstaculo="Hoyo", longitud=50}
+tiroGenerico = UnTiro {velocidad = 10, precision = 95, altura = 0}
+listaDeObstaculos = [obstaculoGenerico1, obstaculoGenerico2, obstaculoGenerico3]
+
+superaTunel :: Tiro -> Bool
+superaTunel tiro = (>90) (precision tiro) && (==0) (altura tiro)
+
+superaLaguna :: Tiro -> Bool
+superaLaguna tiro = velocidad tiro > 80 && between 1 5 (altura tiro)
+
+superaHoyo :: Tiro -> Bool
+superaHoyo tiro = between 5 20 (velocidad tiro) && (==0) (altura tiro) && (>95) (precision tiro)
+
+
+cruzarObstaculo :: Tiro -> Obstaculo -> Tiro
+cruzarObstaculo tiro objeto
+  | criterioDePasoDeUnObstaculo objeto tiro = tiro {velocidad=(velocidad tiro)*2, precision=100, altura=0}
+  | criterioDePasoDeUnObstaculo objeto tiro = tiro {altura= div (altura tiro) (longitud objeto)}
+  | criterioDePasoDeUnObstaculo objeto tiro = tiro {velocidad=0, precision=0, altura=0}
+  | otherwise = tiro {velocidad=0,precision=0,altura=0}
+
+criterioDePasoDeUnObstaculo :: Obstaculo -> Tiro -> Bool
+criterioDePasoDeUnObstaculo (UnObstaculo "Tunel" _) tiro = superaTunel tiro
+criterioDePasoDeUnObstaculo (UnObstaculo "Laguna" _) tiro = superaLaguna tiro
+criterioDePasoDeUnObstaculo (UnObstaculo "Hoyo" _) tiro = superaHoyo tiro
+
+-----------------------------------Punto 4--------------------------------------------------
+palosUtiles :: Jugador -> Obstaculo -> [Palo]
+palosUtiles jugador obstaculo = filter (paloSirve obstaculo jugador) listaDePalos         -- como no se puede mostrar en consola una funcion, cree la otra versio que devuelve
+                                                                                          -- la lista de los palos que cumplen la condicion pero en booleano
+paloSirve :: Obstaculo -> Jugador -> Palo -> Bool
+paloSirve obstaculo jugador palo = criterioDePasoDeUnObstaculo obstaculo (palo (habilidad jugador))
+
+
+
+palosUtiles' :: Jugador -> Obstaculo -> [Bool]
+palosUtiles' jugador obstaculo = map (paloSirve obstaculo jugador) listaDePalos
+
+paloSirve' :: Obstaculo -> Jugador -> Palo -> Bool
+paloSirve' obstaculo jugador palo = criterioDePasoDeUnObstaculo obstaculo (palo (habilidad jugador))
+
+
+
+superarObstaculosConsecutivos :: Tiro -> [Obstaculo] -> Int
+superarObstaculosConsecutivos tiro (cabeza:cola)
+  | null (cabeza:cola) = 0
+  | criterioDePasoDeUnObstaculo cabeza tiro = 1 + superarObstaculosConsecutivos tiro cola
+  | otherwise = 0
+
+
+
+paloMasUtil :: Jugador -> [Obstaculo] -> Palo
+paloMasUtil jugador listaDeObstaculos = foldl1 (mayorSegun (condicionPaloMasUtil jugador listaDeObstaculos)) listaDePalos
+
+condicionPaloMasUtil ::  Jugador -> [Obstaculo] -> Palo -> Int
+condicionPaloMasUtil jugador listaDeObstaculos palo = superarObstaculosConsecutivos (palo (habilidad jugador)) listaDeObstaculos
+
+-----------------------------------Punto 5--------------------------------------------------
+type Tupla = (Jugador, Integer)
+
+listaDePuntos = [(bart, 5), (rafa, 10), (todd, 15)]
+
+
+winnerWinnerChickenDinner :: [Tupla] -> String
+winnerWinnerChickenDinner listaDePuntos = deTuplaAPadreDelJugador (foldl1 condicionDeWinnerWinnerChickenDinner listaDePuntos)
+
+deTuplaAPadreDelJugador :: Tupla -> String
+deTuplaAPadreDelJugador (jugador, puntos) = padre jugador
+
+condicionDeWinnerWinnerChickenDinner :: Tupla -> Tupla -> Tupla
+condicionDeWinnerWinnerChickenDinner tupla1 tupla2
+  | (max (snd tupla1) (snd tupla2)) == (snd tupla1) = tupla1
+  | otherwise = tupla2
+
+noChikenDinner :: [Tupla] -> [String]
+noChikenDinner listaDePuntos = filter (coincideConGanador listaDePuntos) (map deTuplaAPadreDelJugador listaDePuntos)
+
+coincideConGanador :: [Tupla] -> String -> Bool
+coincideConGanador listaDePuntos nombreDePadrePerdedor = not ((==(winnerWinnerChickenDinner listaDePuntos)) nombreDePadrePerdedor)
+
+
+
+
+
